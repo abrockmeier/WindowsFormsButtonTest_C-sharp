e61c302618119c72df867d42095b9dc133cbe64b
abrockmeier <alexander.brockmeier@ambicotech.com>
2019-01-02 12:02:05 +0100

    v.01
    Testing...

--- WindowsFormsButtonTest_C-sharp/Form1.Designer.cs
+++ WindowsFormsButtonTest_C-sharp/Form1.Designer.cs
@@ -28,13 +28,278 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.RB_Herr = new System.Windows.Forms.RadioButton();
+			this.RB_Frau = new System.Windows.Forms.RadioButton();
+			this.RB_Firma = new System.Windows.Forms.RadioButton();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Spalte1 = new System.Windows.Forms.DataGridViewLinkColumn();
+			this.Spalte2 = new System.Windows.Forms.DataGridViewLinkColumn();
+			this.groupBox2.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// RB_Herr
+			// 
+			this.RB_Herr.AutoSize = true;
+			this.RB_Herr.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.RB_Herr.Location = new System.Drawing.Point(67, 37);
+			this.RB_Herr.Name = "RB_Herr";
+			this.RB_Herr.Size = new System.Drawing.Size(81, 33);
+			this.RB_Herr.TabIndex = 1;
+			this.RB_Herr.Text = "Herr";
+			this.RB_Herr.UseVisualStyleBackColor = true;
+			// 
+			// RB_Frau
+			// 
+			this.RB_Frau.AutoSize = true;
+			this.RB_Frau.Checked = true;
+			this.RB_Frau.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.RB_Frau.Location = new System.Drawing.Point(67, 83);
+			this.RB_Frau.Name = "RB_Frau";
+			this.RB_Frau.Size = new System.Drawing.Size(83, 33);
+			this.RB_Frau.TabIndex = 0;
+			this.RB_Frau.TabStop = true;
+			this.RB_Frau.Text = "Frau";
+			this.RB_Frau.UseVisualStyleBackColor = true;
+			// 
+			// RB_Firma
+			// 
+			this.RB_Firma.AutoSize = true;
+			this.RB_Firma.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.RB_Firma.Location = new System.Drawing.Point(67, 128);
+			this.RB_Firma.Name = "RB_Firma";
+			this.RB_Firma.Size = new System.Drawing.Size(96, 33);
+			this.RB_Firma.TabIndex = 2;
+			this.RB_Firma.Text = "Firma";
+			this.RB_Firma.UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(23, 37);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(98, 21);
+			this.checkBox1.TabIndex = 3;
+			this.checkBox1.Text = "checkBox1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Location = new System.Drawing.Point(23, 83);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(98, 21);
+			this.checkBox2.TabIndex = 4;
+			this.checkBox2.Text = "checkBox2";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Location = new System.Drawing.Point(23, 128);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(98, 21);
+			this.checkBox3.TabIndex = 4;
+			this.checkBox3.Text = "checkBox2";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.radioButton2);
+			this.groupBox2.Controls.Add(this.radioButton1);
+			this.groupBox2.Location = new System.Drawing.Point(120, 298);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(362, 128);
+			this.groupBox2.TabIndex = 5;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "groupBox2";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Checked = true;
+			this.radioButton1.Location = new System.Drawing.Point(39, 37);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(110, 21);
+			this.radioButton1.TabIndex = 0;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "radioButton1";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(39, 78);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(110, 21);
+			this.radioButton2.TabIndex = 0;
+			this.radioButton2.Text = "radioButton1";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.RB_Firma);
+			this.groupBox1.Controls.Add(this.RB_Frau);
+			this.groupBox1.Controls.Add(this.RB_Herr);
+			this.groupBox1.Location = new System.Drawing.Point(15, 62);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(258, 200);
+			this.groupBox1.TabIndex = 6;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Anrede:";
+			// 
+			// panel1
+			// 
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.checkBox3);
+			this.panel1.Controls.Add(this.checkBox2);
+			this.panel1.Controls.Add(this.checkBox1);
+			this.panel1.Location = new System.Drawing.Point(304, 72);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(132, 188);
+			this.panel1.TabIndex = 7;
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(156, 32);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(251, 22);
+			this.dateTimePicker1.TabIndex = 8;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 16;
+			this.listBox1.Items.AddRange(new object[] {
+            "show",
+            "some",
+            "content"});
+			this.listBox1.Location = new System.Drawing.Point(511, 310);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(254, 116);
+			this.listBox1.TabIndex = 9;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "ComboTest1",
+            "ComboTest2",
+            "ComboTest3"});
+			this.comboBox1.Location = new System.Drawing.Point(644, 30);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 24);
+			this.comboBox1.TabIndex = 5;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(644, 73);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+			this.numericUpDown1.TabIndex = 10;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(447, 62);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(170, 160);
+			this.pictureBox1.TabIndex = 11;
+			this.pictureBox1.TabStop = false;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Spalte1,
+            this.Spalte2});
+			this.dataGridView1.Location = new System.Drawing.Point(522, 248);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(190, 56);
+			this.dataGridView1.TabIndex = 12;
+			// 
+			// Spalte1
+			// 
+			this.Spalte1.HeaderText = "Spa1";
+			this.Spalte1.Name = "Spalte1";
+			this.Spalte1.Width = 50;
+			// 
+			// Spalte2
+			// 
+			this.Spalte2.HeaderText = "Spa2";
+			this.Spalte2.Name = "Spalte2";
+			this.Spalte2.Width = 50;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.groupBox2);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.RadioButton RB_Herr;
+		private System.Windows.Forms.RadioButton RB_Frau;
+		private System.Windows.Forms.RadioButton RB_Firma;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewLinkColumn Spalte1;
+		private System.Windows.Forms.DataGridViewLinkColumn Spalte2;
 	}
 }
 
